827ba981d2d1c338f13b6510c63994bf61c30f8f
Eray Yuksel <yukseleray203@gmail.com>
2024-01-16 19:02:18 +0300

    AddedCoordinateSystem

---
+++ Assets/Scripts/CoordinateLabel.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[ExecuteAlways] // bu komut altindaki kodlarin hem duzenleme modunda hemde play modda calismasini saglar 
+public class CoordinateLabel : MonoBehaviour
+{
+    TextMeshPro label;
+    Vector2Int coordinats = new Vector2Int();
+
+    private void Awake()
+    {
+        label = GetComponent<TextMeshPro>();
+        DisplayCoordinates(); // oyunu baslattigimda son konumu gostersin diye 1 kez calistiriyorum
+        UpdateObjectName();
+    }
+    void Update()
+    {
+        if (!Application.isPlaying) // oyun calismiyorsak yaptigimiz her degisikligi aninda gosterecek ancak oyunu baslattigimda hicbir degisikligi goremeyecegim
+        {
+            DisplayCoordinates();
+            UpdateObjectName();
+        }
+    }
+
+    void DisplayCoordinates()
+    {
+        coordinats.x = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x);
+        coordinats.y = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z);
+        label.text = coordinats.x + "," + coordinats.y;
+    }
+    void UpdateObjectName()
+    {
+        transform.parent.name = coordinats.ToString();
+    }
+}
