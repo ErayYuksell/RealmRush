e5e6c7b04f80ddcdf8fbe6c69d4fc61dc8a792ff
Eray Yuksel <yukseleray203@gmail.com>
2024-01-26 12:10:20 +0300

    AddedEnemyMovement

--- Assets/Enemy/EnemyMovement.cs
+++ Assets/Enemy/EnemyMovement.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] List<WayPoint> path = new List<WayPoint>();
-    [SerializeField] float waitTime = 1f;
-  
+    [SerializeField][Range(0f, 5f)] float speed = 1f;
+
     void Start()
     {
         StartCoroutine(FollowPath());
@@ -16,8 +16,18 @@ public class EnemyMovement : MonoBehaviour
     {
         foreach (WayPoint wayPoint in path)
         {
-            transform.position = wayPoint.transform.position;
-            yield return new WaitForSeconds(waitTime);
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = wayPoint.transform.position;
+            float travelPercent = 0f;
+
+            transform.LookAt(endPosition);
+
+            while (travelPercent < 1f) // lerpin en sonundaki t degeri 0 ken daha hareket baslamamis demek 1 de de hedef konuma ulasilmis demek 
+            {
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame(); //frame bitmesini bekleyecek
+            }
         }
     }
 
